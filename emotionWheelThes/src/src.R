193c931e47d8c3b05bdc23907004e211e4044bfe
susmulhearn <pricklypears0515@gmail.com>
2024-06-04 05:27:44 -0500

    trying to separate lyrics by word

---
+++ emotionWheelThes/src/src.R
@@ -0,0 +1,3 @@
+library(tidyverse)      # data manipulation & plotting
+library(stringr)        # text cleaning and regular expressions
+library(tidytext)       # provides additional text mining functions
--- tSSentimentA/src/input.R
+++ tSSentimentA/src/input.R
@@ -1,4 +1,14 @@
-path <- getwd()
+#brief: inputting lyrics and setting up rdata file for sentiment analysis
 
-lyrics <- readLines(con = "Home/R_portfolio/DataSciencePortfolio/tSSentimentA/src/closure.txt")
-print(lyrics)
+path <- "tSSentimentA/src/closure.txt"
+lyrics <- paste(scan(path, what="character", sep= "\n"))
+
+#refining by setting up data frame
+dflyrics<-enframe (lyrics, name = "lineno", value = "lyricline")
+linenum<-aframe[,"lineno"]
+lyricwords=aframe[,"lyricline"]
+dftib=tibble(linenum, lyricwords)
+#creating data frame by each word on a new line
+library(tidyr)
+separate_rows(dflyrics, lyricwords, convert = TRUE)
+df_by_word%>%separate_longer_delim(c(lyricwords), delim = " ")
