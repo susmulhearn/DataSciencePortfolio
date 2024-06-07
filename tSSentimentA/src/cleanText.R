4d8fd0609696d2e3e74768f5c4e04e88b6283005
susmulhearn <pricklypears0515@gmail.com>
2024-06-07 17:33:41 -0500

    pulling out rows with lyrics and chorus listed

--- tSSentimentA/src/cleanText.R
+++ tSSentimentA/src/cleanText.R
@@ -1,10 +1,5 @@
 path <- "tSSentimentA/src/closure.txt"
 
-clean_table(read.table(path))
+clean_table<-read.table(path)
 head(clean_table)
 
-
-
-
-library(stringr)
-str_replace_all(string, pattern, replacement)
--- tSSentimentA/src/input.R
+++ tSSentimentA/src/input.R
@@ -2,11 +2,16 @@
 
 path <- "tSSentimentA/src/closure.txt"
 lyrics <- paste(scan(path, what="character", sep= "\n"))
+
 #refining by setting up data frame
 dflyrics<-enframe (lyrics, name = "lineno", value = "lyricline")
 
 linenum<-dflyrics["lineno"]
 lyricwords=dflyrics[,"lyricline"]
 dftib=tibble(linenum, lyricwords)
-dftib<-dftib[-1, ]
+which(dftib$lyricline == "[Verse 2]")
+clean_lyrics <- dftib[-c(1, 8, 16, 22, 37), ]
+
+
+
 
