e761f480f11c839c1b5dc4347e0967677b8fefa5
susmulhearn <pricklypears0515@gmail.com>
2024-06-08 10:26:16 -0500

    Last line cleaned and added todo: build script to do multiple data files

--- tSSentimentA/src/input.R
+++ tSSentimentA/src/input.R
@@ -13,14 +13,14 @@ dftib=tibble(linenum, lyricwords)
 verse<-which(dftib$lyricline == "[Verse 2]")
 chorus<-which(dftib$lyricline == "[Chorus]")
 remout<- c(verse, chorus)
-clean_lyrics <- dftib[-c(1, ,remout), ]
+clean_lyrics <- dftib[-c(1,remout), ]
 #cleaning last row
 last_row<-clean_lyrics%>%tail(1)
 no_embed=last_row$lyricline=substr(last_row$lyricline, 1, nchar(last_row$lyricline)-5)
-lrow_clean<-gsub('[[:digit:]]+', '', last_row$lyricline)
+clean_var<-gsub('[[:digit:]]+', '', last_row$lyricline)
 #fixing last element
-x<-last_row[1,1]
-
+x=last_row[1,1]
+row_clean_var=tibble(x, lyricline=clean_var)
 #replacing last row
 cl_no_embeddf <- clean_lyrics %>% filter(row_number() <= n()-1)
-cl_complete[nrow(cl_no_embeddf) + 1,] = c(x,lrow_clean)
+cl_final<-rbind(cl_no_embeddf, row_clean_var)
